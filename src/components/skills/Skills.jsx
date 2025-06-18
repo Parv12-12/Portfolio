57a697c520a932b8fab29b76ed0056c638a59113
Parv Goswami <parvgoswami833@gmail.com>
2025-06-18 18:52:07 +0530

    Add files via upload

---
+++ src/components/skills/Skills.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import styles from "./Skills.module.css";
+import data from "../../data/SkillsData"; 
+
+
+
+export const Skills = () => {
+  return (
+    <section className={styles.container} id="experience">
+      <h2 className={styles.title}>Skills</h2>
+      <div className={styles.content}>
+        <div className={styles.skills}>
+          {data.map((skill, id) => {
+            return (
+              <div key={id} className={styles.skill}>
+                <div className={styles.skillImageContainer}>
+                  <img src={skill.imageSrc} alt={skill.title} />
+                </div>
+                <p>{skill.title}</p>
+              </div>
+            );
+          })}
+        </div>
+       
+      </div>
+    </section>
+  );
+};
+export default Skills
