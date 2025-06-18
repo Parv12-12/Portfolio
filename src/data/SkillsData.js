3153bd885415a89dbf1db881c4ee34224447faf6
Parv Goswami <parvgoswami833@gmail.com>
2025-06-18 18:56:41 +0530

    Add files via upload

---
+++ src/data/SkillsData.js
@@ -0,0 +1,48 @@
+import css from "..//assets/skills/css.png"
+import html from  '..//assets/skills/html.png'
+import react from  '..//assets/skills/react.png'
+import javascript from  '..//assets/skills/javascript.png'
+import tailwind from  '..//assets/skills/tailwind.png'
+import redux from  '..//assets/skills/redux.png'
+import sql from  '..//assets/skills/sql.png'
+import bootstrap from  '..//assets/skills/bootstrap.png'
+const data = 
+[
+  {
+    "title": "HTML",
+    "imageSrc": html
+  },
+  {
+    "title": "CSS",
+    "imageSrc": css
+  },
+   {
+    "title": "React",
+    "imageSrc": react
+  },
+ 
+  {
+    "title": "JavaScript",
+    "imageSrc": javascript
+  },
+  {
+    "title": "Sql",
+    "imageSrc": sql
+  },
+  {
+    "title": "Redux",
+    "imageSrc": redux
+  },
+  {
+    "title": "BootStrap",
+    "imageSrc": bootstrap
+  },
+   {
+    "title": "TailwindCSS",
+    "imageSrc": tailwind
+  },
+ 
+ 
+]
+
+export default data
---
+++ src/data/projects.js
@@ -0,0 +1,18 @@
+const project = [
+  {
+    "title": "Moveie App",
+    "imageSrc": "projects/project.png",
+    "description": "This is a project made to search the movies and web series.",
+    "skills": ["React", "Html", "css"],
+    "demo": "https://searchmovi.netlify.app/",
+    "source": "https://github.com/Parv12-12/Movies-Website"
+  },
+  {
+    "title": "Tech News",
+    "description": "This is a project made to search the recent techninal news.",
+    "skills": ["React", "Html", "css"],
+    "demo": "https://gotechnews.netlify.app/",
+    "source": "https://github.com/Parv12-12/Tech-News-website"
+  },
+]
+export default project
